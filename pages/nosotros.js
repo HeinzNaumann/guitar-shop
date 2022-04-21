a39c217c23358bf66084b5a751dc3e039087337a
Heinz <hlanaumann@gmail.com>
2022-04-21 11:26:32 +0200

    añadimos los primeros cambios

---
+++ pages/nosotros.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Nosotros = () => {
+  return (
+
+      <Layout>
+        <h1>Desde Nosotros</h1>
+        <Link href="/">Ir a inicio</Link>
+      </Layout>
+
+  );
+};
+
+export default Nosotros;
